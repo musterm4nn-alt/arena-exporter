4e9b5b04daf20c96ae79cda65b2a47d372770b08
musterm4nn-alt <mannmaxi@b-tu.de>
2026-09-07 23:54:49 +0200

    Fix 2.1.3: battles.js loader imports p1+p2a1+p2a2+p2b1+p2b2+p2

--- src/battles.js
+++ src/battles.js
@@ -1,3 +1,7 @@
 /* Temporary split loader (GitHub restore). */
 importScripts("src/battles.p1.js");
+importScripts("src/battles.p2a1.js");
+importScripts("src/battles.p2a2.js");
+importScripts("src/battles.p2b1.js");
+importScripts("src/battles.p2b2.js");
 importScripts("src/battles.p2.js");
